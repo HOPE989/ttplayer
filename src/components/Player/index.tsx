b1aadc3ba4e082986c130d3d7378ffaacf737979
HOPE <836495061@qq.com>
2025-01-08 00:13:46 +0800

    修复初始化浮块没显示的bug，来源：canvas组件没设置id

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -15,7 +15,7 @@ const Player = React.forwardRef((props: Props, audioRef: Ref<HTMLAudioElement>) 
     return (
         <div className={styles.player}>
             <div className={styles.canvas}>
-                <canvas width={500} height={300}/>
+                <canvas id="canvas" width={500} height={300}/>
             </div>
             <div className={styles.controls}>
                 <audio ref={audioRef}
